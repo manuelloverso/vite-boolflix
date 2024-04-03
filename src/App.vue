23dc59d80dffd550e66a8008537a3d0f532be15b
manuelloverso <manuelloverso@gmail.com>
2024-04-03 14:45:16 +0200

    add series api call

--- src/App.vue
+++ src/App.vue
@@ -11,23 +11,42 @@ export default {
   data() {
     return {
       store,
-      movies: [],
     };
   },
 
-  created() {},
+  mounted() {},
 };
 </script>
 <template>
   <div class="search-bar">
     <input v-model="store.search" type="text" />
-    <button @click="store.getMovies">Click Me</button>
+    <button @click="store.callApi">Click Me</button>
+    <h1>Movies</h1>
     <ul>
       <li v-for="movie in store.movies">
         {{ movie.title }}
         {{ movie.original_title }}
         {{ movie.original_language }}
         {{ movie.vote_average }}
+        <img
+          width="50"
+          :src="`http://purecatamphetamine.github.io/country-flag-icons/3x2/${movie.original_language.toUpperCase()}.svg`"
+          alt=""
+        />
+      </li>
+    </ul>
+    <h1>Series</h1>
+    <ul>
+      <li v-for="singleSeries in store.series">
+        {{ singleSeries.name }}
+        {{ singleSeries.original_name }}
+        {{ singleSeries.original_language }}
+        {{ singleSeries.vote_average }}
+        <img
+          width="50"
+          :src="`http://purecatamphetamine.github.io/country-flag-icons/3x2/${singleSeries.original_language.toUpperCase()}.svg`"
+          alt=""
+        />
       </li>
     </ul>
   </div>
--- src/store.js
+++ src/store.js
@@ -3,15 +3,24 @@ import axios from "axios";
 
 export const store = reactive({
   movies: [],
+  series: [],
   search: "",
 
-  getMovies() {
+  callApi() {
     axios
       .get(
         `https://api.themoviedb.org/3/search/movie?api_key=84160a7353d1d37c7ead96a2fcac030a&query=${this.search}`
       )
       .then((response) => {
         this.movies = response.data.results;
+      });
+
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/tv?api_key=84160a7353d1d37c7ead96a2fcac030a&query=${this.search}`
+      )
+      .then((response) => {
+        this.series = response.data.results;
       });
   },
 });
