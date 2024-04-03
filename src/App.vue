cfbad1bd3dc233aade296815025d9779c17a4b9b
manuelloverso <manuelloverso@gmail.com>
2024-04-03 11:16:32 +0200

    init commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import MovieCard from "./components/MovieCard.vue";
+export default {
+  name: "App",
+  components: {
+    MovieCard,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<template>
+  <MovieCard />
+</template>
+<style scoped></style>
---
+++ src/components/MovieCard.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: "MovieCard",
+  data() {
+    return {};
+  },
+};
+</script>
+<template>
+  <h1>test</h1>
+</template>
+<style scoped></style>
