a09e52af36212e1fffb4c4f9d4d74f47ce55c6d0
manuelloverso <manuelloverso@gmail.com>
2024-04-04 12:49:57 +0200

    add content showing on card hover

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -11,6 +11,7 @@ export default {
   data() {
     return {
       store,
+      isHover: false,
     };
   },
 
@@ -34,13 +35,23 @@ export default {
 };
 </script>
 <template>
-  <li v-if="movie.poster_path != null" class="movie-card">
+  <li
+    v-if="movie.poster_path != null"
+    class="movie-card"
+    :class="{ reveal: isHover }"
+    @mouseover="isHover = true"
+    @mouseleave="isHover = false"
+  >
+    <!-- Showed movie poster -->
     <div class="showed">
       <img
+        class="poster-img"
         :src="`https://image.tmdb.org/t/p/w342${movie.poster_path}`"
         alt=""
       />
     </div>
+
+    <!-- Hidden Content which shows on hover -->
     <div class="hidden">
       <p><span>Title: </span>{{ title }}</p>
       <p><span>Title: </span>{{ originalTitle }}</p>
@@ -56,37 +67,59 @@ export default {
         ></i>
       </p>
       <p><span>Overview: </span>{{ movie.overview }}</p>
+      <div>
+        <span>Language: </span>
+        <img
+          width="40"
+          :src="`http://purecatamphetamine.github.io/country-flag-icons/3x2/${handleLanguage(
+            movie.original_language
+          )}.svg`"
+          alt=""
+        />
+      </div>
     </div>
   </li>
 </template>
 <style scoped>
 .movie-card {
-  box-shadow: black 0 0 20px 3px;
   width: calc((100% / 6) - 17px);
-  & img {
+  position: relative;
+
+  .poster-img {
     display: block;
     width: 100%;
-    height: 440px; /* @todo: fix to height 100% */
+    height: 100%;
     object-fit: cover;
   }
 }
+
+.hidden,
+.showed {
+  backface-visibility: hidden;
+  transition: transform 0.5s;
+  box-shadow: black 0 0 20px 3px;
+}
+
+.hidden {
+  transform: rotateY(-180deg);
+  height: 100%;
+  overflow-y: auto;
+  position: absolute;
+  top: 0;
+}
+
+.showed {
+  transform: rotateY(0);
+  height: 100%;
+}
+
+.reveal .hidden,
+.matched .hidden {
+  transform: rotateY(0);
+}
+
+.reveal .showed,
+.matched .showed {
+  transform: rotateY(-180deg);
+}
 </style>
-<!-- {{ title }}
-    {{ originalTitle }}
-    {{ movie.original_language }}
-    {{ movie.vote_average }}
-    <i
-      v-for="n in divideForTwo(movie.vote_average)"
-      class="fa-solid fa-star"
-    ></i>
-    <i
-      v-for="n in 5 - divideForTwo(movie.vote_average)"
-      class="fa-regular fa-star"
-    ></i>
-    <img
-      width="50"
-      :src="`http://purecatamphetamine.github.io/country-flag-icons/3x2/${handleLanguage(
-        movie.original_language
-      )}.svg`"
-      alt=""
-    /> -->
